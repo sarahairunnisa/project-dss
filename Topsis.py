9d13c2cb824f703ad853abc7bff3a8a818f33a65
sarahairunnisa <sarah.kp1005@gmail.com>
2023-12-12 15:28:36 +0700

    pindah repo

---
+++ Topsis.py
@@ -0,0 +1,69 @@
+from math import sqrt
+import re
+
+class Topsis:
+    def __init__(self, data, weights, impacts) -> None:
+        self.data = data
+        self.features = {
+            'Processor': 'Processor',
+            'Screen': 'Screen',
+            'InternalMemory': 'InternalMemory',
+            'TotalStorage': 'TotalStorage',
+            'Weight': 'Weight',
+            'BatteryCells': 'BatteryCells',
+        }
+        self.weights = weights
+        self.impacts = impacts
+        self.ideal = {}
+        self.negativeIdeal = {}
+
+    def euclideanDistance(self, data) -> float:
+        return sqrt(sum([i ** 2 for i in data]))
+
+    def normalize(self) -> None:
+        for key, value in self.features.items():
+            self.data[value + 'Normalized'] = self.data[key] / self.euclideanDistance(self.data[key])
+            self.data[value + 'Normalized'] *= self.weights[list(self.features.keys()).index(key)]
+
+    def idealNegativeSolution(self) -> None:
+        for key, value in self.features.items():
+            if self.impacts[list(self.features.keys()).index(key)] == 1:
+                self.ideal[value] = self.data[value + 'Normalized'].max()
+                self.negativeIdeal[value] = self.data[value + 'Normalized'].min()
+            elif self.impacts[list(self.features.keys()).index(key)] == 0:
+                self.ideal[value] = self.data[value + 'Normalized'].min()
+                self.negativeIdeal[value] = self.data[value + 'Normalized'].max()
+    
+    def separationMeasures(self) -> None:
+        for _, value in self.features.items():
+            self.data[value + 'SeparationMeasureIdeal'] = pow(self.data[value + 'Normalized'] - self.ideal[value], 2)
+            self.data[value + 'SeparationMeasureNegativeIdeal'] = pow(self.data[value + 'Normalized'] - self.negativeIdeal[value], 2)
+        
+        self.data['SeparationMeasureIdeal'] = self.data[[f'{value}SeparationMeasureIdeal' for value in self.features.values()]].sum(axis=1)
+        self.data['SeparationMeasureNegativeIdeal'] = self.data[[f'{value}SeparationMeasureNegativeIdeal' for value in self.features.values()]].sum(axis=1)
+
+        self.data['SeparationMeasureIdeal'] = self.data['SeparationMeasureIdeal'].apply(sqrt)
+        self.data['SeparationMeasureNegativeIdeal'] = self.data['SeparationMeasureNegativeIdeal'].apply(sqrt)
+
+    def performanceScore(self) -> None:
+        self.data['totalRelativeCloseness'] = self.data['SeparationMeasureNegativeIdeal'] / (self.data['SeparationMeasureIdeal'] + self.data['SeparationMeasureNegativeIdeal'])
+
+    def rank(self) -> None:
+        self.data = self.data.sort_values(by=['totalRelativeCloseness'], ascending=False)
+
+    def run(self) -> None:
+        self.normalize()
+        self.idealNegativeSolution()
+        self.separationMeasures()
+        self.performanceScore()
+        self.rank()
+    
+    def getEmbed(self) -> None:
+        regex = re.compile(r'https://www.laptoparena.net/product/(\w+)')
+        embed = []
+        for i in range(15):
+            embed.append(regex.sub(self.data['ImageURLs'].iloc[i], self.data['ImageURLs'].iloc[i]))
+        return embed
+    
+    def getPercentage(self) -> None:
+        return self.data['totalRelativeCloseness'].iloc[0:15]
---
+++ main.py
@@ -0,0 +1,104 @@
+import re
+import pandas as pd
+import streamlit as st
+from streamlit_float import *
+from Topsis import Topsis
+import os
+
+def display_recommendation(iframe_code, percentage):
+    st.markdown(f"This laptop matches your preferences by **{percentage * 100:.3f}%**")
+    st.progress(percentage)
+    st.markdown(iframe_code, unsafe_allow_html=True)
+
+def main():
+    dataset_filename = os.path.abspath("data/Laptop.csv")
+    data = None  # Initialize data outside the if conditions
+
+    if 'searched' not in st.session_state:
+        st.session_state.searched = False
+
+    if 'clicked' not in st.session_state:
+        st.session_state.clicked = False
+
+    def click_button():
+        st.session_state.clicked = True
+
+    if 'preVal_w' and 'preVal_i' not in st.session_state:
+        st.session_state.preVal_w = [0] * 7
+        st.session_state.preVal_i = [0] * 7
+
+    if 'val_w' and 'val_i' not in st.session_state:
+        st.session_state.val_w = [0] * 7
+        st.session_state.val_i = [0] * 7
+
+    weight = [0] * 7
+    impact = [0] * 7
+    st.markdown("<h1 style='text-align: center; color: black;'>Rekomendasi <span style='color: #e69ded;'>Laptop</span> Menggunakan Metode TOPSIS</h1>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: center; color: black;'>oleh <b>Aliya Rahmania</b> (210005), <b>Adinda Salsabila</b> (210017), <b>Sarah Khairunnisa Prihantoro</b> (210063), <b>Zakia Noorardini</b> (210065) sebagai Project UAS Mata Kuliah Decision Support System</p>", unsafe_allow_html=True)
+
+    attributes = ["Processor", "Screen", "InternalMemory", "TotalStorage", "Weight", "BatteryCells", "PointingDevice"]
+    descriptions = [
+        "Provides the instructions and processing power the computer needs to do its work.",
+        "Screen size and quality.",
+        "Internal memory (RAM) size.",
+        "Total storage capacity.",
+        "Weight of the laptop.",
+        "Number of battery cells.",
+        "Type of pointing device (e.g., touchpad)."
+    ]
+
+    for i in range(len(attributes)):
+        st.sidebar.markdown(f"<h4 style='color: #1DB954; font-weight: bold;'>{attributes[i]}</h4>", unsafe_allow_html=True)
+        weight[i] = st.sidebar.slider(descriptions[i], -10, 10, 0)
+        impact[i] = 1 if weight[i] >= 0 else 0
+        weight[i] = abs(weight[i])
+        st.session_state.preVal_w[i] = weight[i]
+        st.session_state.preVal_i[i] = impact[i]
+    st.sidebar.markdown("***")
+
+    float_init()
+    float_container = st.sidebar.container()
+    with float_container:
+        if weight != [0] * 7:
+            button = float_container.button("Search", on_click=click_button, disabled=False, use_container_width=True)
+        else:
+            error = float_container.error("Please change at least one preference!")
+            button = float_container.button("Search", on_click=click_button, disabled=True, use_container_width=True)
+
+    float_container.float("bottom: 0; background-color: #f0f2f6; padding: 0px 0px 20px")
+
+    if st.session_state.clicked == True:
+        data = pd.read_csv(dataset_filename)
+        topsis = Topsis(data, st.session_state.preVal_w, st.session_state.preVal_i)
+
+        for i in range(0, 7):
+            st.session_state.val_w[i] = st.session_state.preVal_w[i]
+            st.session_state.val_i[i] = st.session_state.preVal_i[i]
+
+        st.session_state.clicked = False
+        st.session_state.searched = True
+
+    if st.session_state.searched == False:
+        st.markdown("<h4 style='text-align: center; color: black;'>↖️ You haven't searched for a laptop yet, head to the sidebar!</h4>", unsafe_allow_html=True)
+        st.markdown("<h4 style='text-align: center; color: black;'>How about a recommendation for you 👇</h4>", unsafe_allow_html=True)
+        # Assuming you want to display a sample recommendation when not searched
+        if data is not None:
+            sample_columns = ["Processor", "Screen", "InternalMemory", "TotalStorage", "Weight", "BatteryCells", "PointingDevice"]
+            sample = data[sample_columns].iloc[[0]]
+            st.write("Sample Recommendation:")
+            st.write(sample)  # Display sample laptop details
+    else:
+        if data is not None:
+            data = pd.read_csv(dataset_filename)
+            topsis = Topsis(data, st.session_state.val_w, st.session_state.val_i)
+            topsis.run()
+            rec = topsis.getEmbed()
+            percent = topsis.getPercentage()
+
+            for i, percentage in zip(rec, percent):
+                iframe_code = f'<iframe style="border-radius:12px; background-color: transparent;" src="{i}?utm_source=generator" width="100%" height="160" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+                st.markdown(iframe_code, unsafe_allow_html=True)
+                st.write(f'Recommendation Percentage: {percentage}')
+
+if __name__ == '__main__':
+    main()
