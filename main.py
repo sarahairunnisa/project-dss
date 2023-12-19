38280ba56c42399e99750bc90e98f5a87859c706
sarahairunnisa <sarah.kp1005@gmail.com>
2023-12-19 18:35:06 +0700

    update

--- main.py
+++ main.py
@@ -28,30 +28,30 @@ def main():
 
     weight = [0] * 6
     impact = [0] * 6
-    st.markdown("<h1 style='text-align: center; color: black;'>Rekomendasi <span style='color: #e69ded;'>Laptop</span> Menggunakan Metode TOPSIS</h1>", unsafe_allow_html=True)
-    st.markdown("<p style='text-align: center; color: black;'>oleh <b>Aliya Rahmania</b> (210005), <b>Adinda Salsabila</b> (210017), <b>Sarah Khairunnisa Prihantoro</b> (210063), <b>Zakia Noorardini</b> (210065) sebagai Project UAS Mata Kuliah Decision Support System</p>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center; color: black;'>Sistem Pendukung Keputusan <span style='color: #ab2b7e;'> Pemilihan Laptop</span> Menggunakan Metode <span style='color: #2f359e;'> TOPSIS</span></h1>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: center; color: black;'>oleh <b>Aliya Rahmania</b> (210005), <b>Adinda Salsabila</b> (210017), <b>Sarah Khairunnisa Prihantoro</b> (210063), dan <b>Zakia Noorardini</b> (210065) sebagai Project UAS Mata Kuliah Decision Support System</p>", unsafe_allow_html=True)
 
-    attributes = ["Processor💽", "Screen💻", "InternalMemory🖥️", "TotalStorage💾", "Weight⚖️", "BatteryCells🔋"]
+    attributes = ["Prosesor 💽", "Layar 💻", "Memori Internal 🖥️", "Penyimpanan 💾", "Berat ⚖️", "Baterai 🔋"]
     descriptions = [
-        "Provides the instructions and processing power the computer needs to do its work.",
-        "Screen size and quality.",
-        "Internal memory (RAM) size.",
-        "Total storage capacity.",
-        "Weight of the laptop.",
-        "Numbers of battery cells.",
+        "Menyediakan instruksi dan daya pemrosesan yang diperlukan oleh komputer untuk menjalankan tugasnya.",
+        "Ukuran dan kualitas layar.",
+        "Ukuran memori internal (RAM).",
+        "Kapasitas penyimpanan total.",
+        "Berat laptop.",
+        "Jumlah sel baterai.",
     ]
 
-    st.sidebar.markdown(f"<h4 style='color: #1DB954; font-weight: bold;'>{attributes[0]}</h4>", unsafe_allow_html=True)
+    st.sidebar.markdown(f"<h4 style='color: #ab2b7e; font-weight: bold;'>{attributes[0]}</h4>", unsafe_allow_html=True)
     weight[0] = st.sidebar.slider(descriptions[0], min_value=1.3, max_value=3.1, step=0.1, key='0')
-    st.sidebar.markdown(f"<h4 style='color: #1DB954; font-weight: bold;'>{attributes[1]}</h4>", unsafe_allow_html=True)
+    st.sidebar.markdown(f"<h4 style='color: #ab2b7e; font-weight: bold;'>{attributes[1]}</h4>", unsafe_allow_html=True)
     weight[1] = st.sidebar.slider(descriptions[1], min_value=12.5, max_value= 17.3, step=0.1, key='1')
-    st.sidebar.markdown(f"<h4 style='color: #1DB954; font-weight: bold;'>{attributes[2]}</h4>", unsafe_allow_html=True)
+    st.sidebar.markdown(f"<h4 style='color: #ab2b7e; font-weight: bold;'>{attributes[2]}</h4>", unsafe_allow_html=True)
     weight[2] = st.sidebar.slider(descriptions[2], min_value=4, max_value=32, step=4, key='2')
-    st.sidebar.markdown(f"<h4 style='color: #1DB954; font-weight: bold;'>{attributes[3]}</h4>", unsafe_allow_html=True)
+    st.sidebar.markdown(f"<h4 style='color: #ab2b7e; font-weight: bold;'>{attributes[3]}</h4>", unsafe_allow_html=True)
     weight[3] = st.sidebar.slider(descriptions[3], min_value=128, max_value= 2256, step=4, key='3')
-    st.sidebar.markdown(f"<h4 style='color: #1DB954; font-weight: bold;'>{attributes[4]}</h4>", unsafe_allow_html=True)
+    st.sidebar.markdown(f"<h4 style='color: #ab2b7e; font-weight: bold;'>{attributes[4]}</h4>", unsafe_allow_html=True)
     weight[4] = st.sidebar.slider(descriptions[4], min_value=1.2,max_value= 4.42, step=0.01,key='4')
-    st.sidebar.markdown(f"<h4 style='color: #1DB954; font-weight: bold;'>{attributes[5]}</h4>", unsafe_allow_html=True)
+    st.sidebar.markdown(f"<h4 style='color: #ab2b7e; font-weight: bold;'>{attributes[5]}</h4>", unsafe_allow_html=True)
     weight[5] = st.sidebar.slider(descriptions[5], min_value=3, max_value=6, step=1, key='5')
 
     for i in range(len(attributes)):
@@ -84,14 +84,14 @@ def main():
         st.session_state.searched = True
 
     if st.session_state.searched == False:
-        st.markdown("<h4 style='text-align: center; color: black;'>↖️ You haven't searched for a laptop yet, head to the sidebar!</h4>", unsafe_allow_html=True)
-        st.markdown("<h4 style='text-align: center; color: black;'>How about a recommendation for you 👇</h4>", unsafe_allow_html=True)
-        # Assuming you want to display a sample recommendation when not searched
-        if data is not None:
-            sample_columns = ["Processor", "Screen", "InternalMemory", "TotalStorage", "Weight", "BatteryCells"]
-            sample = data[sample_columns].iloc[[0]]
-            st.write("Sample Recommendation:")
-            st.write(sample)  # Display sample laptop details
+        st.markdown("<h4 style='text-align: center; color: black;'>⬅️ Anda belum mencari laptop, beralihlah ke sidebar!</h4>", unsafe_allow_html=True)
+        # st.markdown("<h4 style='text-align: center; color: black;'>How about a recommendation for you 👇</h4>", unsafe_allow_html=True)
+        # # Assuming you want to display a sample recommendation when not searched
+        # if data is not None:
+        #     sample_columns = ["Processor", "Screen", "InternalMemory", "TotalStorage", "Weight", "BatteryCells"]
+        #     sample = data[sample_columns].iloc[[0]]
+        #     st.write("Sample Recommendation:")
+        #     st.write(sample)  # Display sample laptop details
     else:
         if data is not None:
             data = pd.read_csv('data/Laptop.csv')
@@ -105,7 +105,7 @@ def main():
                 iframe_code = f'<iframe style="border-radius:12px; background-color: transparent;" src="{i}?utm_source=generator" width="100%" height="160" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
                 st.markdown(iframe_code, unsafe_allow_html=True)
                 #st.write(f'Laptop Recommendation: {name}')
-                st.write(f'Recommendation Percentage: {percentage * 100:.3f}')
+                st.write(f'Persentase Rekomendasi: {percentage * 100:.3f}')
 
 if __name__ == '__main__':
     main()
